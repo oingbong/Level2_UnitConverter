a7fcee1e45e9c44501e98d63043a6dc8fb76f0eb
oingbong <oingbong@gmail.com>
2018-07-13 17:59:59 +0900

    update Method for WeightUnit

--- UnitConverter/main.swift
+++ UnitConverter/main.swift
@@ -14,17 +14,19 @@
  4. 종료키
  5. enum 사용
  6. CharacterSet 응용하여 함수 만듬
+ 7. 문자열 나누는 함수 & 단위 구하는 함수 통합 -> divideUnit
+ 9. compareUnit 함수 더 줄이는 방법?
 */
 
 import Foundation
 
 // 인치 길이 변환과 예외 처리
 
-typealias StringTuple = (first: String, last: String, index: Int)
 typealias UnitTuple = (first: String, last: String, value: Double)
 
 // 값 받기
 func main() -> Bool {
+    
     print("값을 입력하세요 ex : 18cm inch // 종료는 q 혹은 quit")
     let inputValue = readLine()
     
@@ -34,8 +36,7 @@ func main() -> Bool {
     }
     
     if let input = inputValue {
-        let divide: StringTuple = divideUnit(input: input)
-        let extract: UnitTuple = extractUnit(StringTuple: divide)
+        let extract: UnitTuple = divideUnit(input: input)
         compareUnit(UnitTuple: extract)
     }
 
@@ -49,85 +50,70 @@ enum LengthUnit:Double{
     case Yard = 91.44
 }
 
-// 문자열 나누기
-func divideUnit(input:String) -> StringTuple {
+enum WeightUnit:Double{
+    case G = 1
+    case Kg = 1000
+    case Oz = 28.34952
+    case Lb = 453.59237
+}
+
+func divideUnit(input:String) -> UnitTuple {
     let separatorStr = input.split(separator: " ")
     let firstStr:String = String(separatorStr.first!)
     let lastStr:String = String(separatorStr.last!)
     
-    let index:Int = getIndex(characters: firstStr)
-    let result: StringTuple = (firstStr, lastStr, index)
-    
-    return result
-}
-
-func getIndex(characters:String) -> Int {
     let characterSet:String = "abcdefghijklmnopqrstuvwxyz"
-
-    var characters:String = characters
-    // startindex값부터 순서대로 확인후 characterSet에 포함되지 않은 경우 지워주고 포함되면 멈춥니다.
-    for character in characters {
-        if characterSet.contains(character){
-            break
+    
+    var valueString:String = ""
+    var firstUnit:String = ""
+    for char in firstStr {
+        if !characterSet.contains(char){
+            valueString.append(char)
         }else{
-            characters.remove(at: characters.startIndex)
+            firstUnit.append(char)
         }
     }
     
-    // ex : 2 -> -2 / 4 -> -4 convert
-    let index:Int = characters.count - 2 * characters.count
-    return index
-}
-
-// Number, Unit 구하기
-func extractUnit(StringTuple: StringTuple) -> UnitTuple {
-    
-    let firstStr = StringTuple.first
-    let lastStr = StringTuple.last
-    let index = StringTuple.index
-    
-    // 숫자 추출 및 Double로 변환
-    let indexEndOfText = firstStr.index(firstStr.endIndex, offsetBy: index)
-    let beforeNumber = firstStr[..<indexEndOfText]
-    let doubleNumber:Double = Double(beforeNumber)!
-    
-    // 첫 번째 Unit 구하기
-    let numberIndex = firstStr.count + index
-    let indexStartOfText = firstStr.index(firstStr.startIndex, offsetBy: numberIndex)
-    let firstUnit:String = String(firstStr[indexStartOfText...])
+    // TypeCasting : String -> Double
+    let value:Double = Double(valueString)!
     
     // 두 번째 Unit 구하기 : 두번째 값이 입력받지 않은 경우 firstUnit으로 세팅합니다.
     let lastUnit:String = firstStr == lastStr ? firstUnit : lastStr
     
-    // 리턴값
-    let result:UnitTuple = (firstUnit, lastUnit, doubleNumber)
+    let result:UnitTuple = (firstUnit, lastUnit, value)
     return result
 }
 
-func convertUnit(unit:String, value:Double, isCM:Bool) -> Double{
-    // isCM = true  -> *
-    // isCM = false -> /
-    var number:Double = 0
+func calculateUnit(unit: String, value: Double, isDefault: Bool) -> Double {
+    let number:Double = settingValueOfDivide(str: unit)
+    return calculation(value: value, number: number, isTrue: isDefault)
+}
+
+func settingValueOfDivide(str:String) -> Double{
     var result:Double = 0
-    
-    switch unit {
-    case "m":
-        number = LengthUnit.M.rawValue
-    case "inch":
-        number = LengthUnit.Inch.rawValue
-    case "yard":
-        number = LengthUnit.Yard.rawValue
-    default:
-        number = LengthUnit.Cm.rawValue
+    switch str {
+    case "g": result = WeightUnit.G.rawValue
+    case "kg": result = WeightUnit.Kg.rawValue
+    case "oz": result = WeightUnit.Oz.rawValue
+    case "lb": result = WeightUnit.Lb.rawValue
+        
+    case "cm": result = LengthUnit.Cm.rawValue
+    case "m": result = LengthUnit.M.rawValue
+    case "inch": result = LengthUnit.Inch.rawValue
+    case "yard": result = LengthUnit.Yard.rawValue
+        
+    default: result = 1
     }
-    
-    if isCM {
-        result = value * number
-    }else{
-        result = value / number
-    }
-    
+        
     return result
+}
+
+func calculation(value:Double, number:Double, isTrue:Bool) -> Double {
+    if isTrue {
+        return value * number
+    }else {
+        return value / number
+    }
 }
 
 // Unit 비교
@@ -138,33 +124,27 @@ func compareUnit(UnitTuple:UnitTuple){
     
     var result:Double = 0
     
-    // 1) cm 으로 변환하거나 건너뛰거나
-    if firstUnit == "cm" {
-        // 그대로 세팅
-        result = value
-    }else{
-        // cm 로 변환 (Default)
-        result = convertUnit(unit: firstUnit, value: value, isCM: true)
-    }
+    // 1. Default Calculate : length -> cm / weight -> g
+    result = calculateUnit(unit: firstUnit, value: value, isDefault: true)
     
-    // 2) last 값에 따라 분기
+    let length:String = "mcminchyard"
+    let weight:String = "gkgozlb"
+    
+    // 2. want to change : -> ?
     if firstUnit == lastUnit {
-        // m -> cm or !m -> m
-        if firstUnit == "m" {
-            // 그대로 위에서 나온 값 전달
-            // lastUnit을 cm 으로 지정
+        if length.contains(firstUnit) && firstUnit == "m" {
             lastUnit = "cm"
-        }else{
-            // unit을 m 으로 지정해서 변환
-            // lastUnit을 m 으로 지정
-            result = convertUnit(unit: "m", value: result, isCM: false)
+        }else if length.contains(firstUnit) && firstUnit != "m" {
+            result = calculateUnit(unit: "m", value: result, isDefault: false)
             lastUnit = "m"
+        }else if weight.contains(firstUnit) && firstUnit == "kg" {
+            lastUnit = "g"
+        }else if weight.contains(firstUnit) && firstUnit != "kg" {
+            result = calculateUnit(unit: "kg", value: result, isDefault: false)
+            lastUnit = "kg"
         }
-    }else if lastUnit == "cm" {
-        // 그대로 위에서 나온 값 전달
-    }else{
-        // last 값에 따라 변환
-        result = convertUnit(unit: lastUnit, value: result, isCM: false)
+    } else {
+        result = calculateUnit(unit: lastUnit, value: result, isDefault: false)
     }
     
     print("\(result)\(lastUnit)")
